fd3f213ebd18fbf7451adddddd6ec3a34e82713f
ycy-ycy <2923616405@qq.com>
2020-04-01 10:29:37 +0800

    Initial

---
+++ js/test.js
@@ -0,0 +1,10 @@
+// var a = [];
+// for (var i = 0; i < 10; i++) {
+//     var b = [];
+//     for (var j = 0; j < 10; j++) {
+//         b[j] = i * 10 + j + 1;
+//     }
+//     a[i] = b;
+// }
+// console.log(a);
+console.log("Hello world")
